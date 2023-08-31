411e4e47e347eb46b5545051620538e33a8ae812
vinay19211 <113415948+vinay19211@users.noreply.github.com>
2023-08-31 08:34:33 +0530

    Create exp9.c

---
+++ exp9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char s[100];
+	printf("enter the string:");
+	scanf("%s",&s);
+    if(s[0]=='0'&&s[strlen(s-1)=='1'])
+    	printf("accepted");
+	else
+	printf("not accepted");
+return 0;
+}
