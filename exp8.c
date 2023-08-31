4e0461ce9fdb3b890519a6ae6953aefa2c84b7c2
vinay19211 <113415948+vinay19211@users.noreply.github.com>
2023-08-31 08:33:59 +0530

    Create exp8.c

---
+++ exp8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char s[100];
+	printf("enter the string:");
+	scanf("%s",&s);
+    if(s[0]=='b'&&s[strlen(s-1)=='a'])
+    	printf("accepted");
+	else
+	printf("not accepted");
+return 0;
+}
